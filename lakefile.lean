9473f2bf2b1c97858956abcaf282a68cbd9a05bc
Alok Singh <alokbeniwal@gmail.com>
2024-07-01 18:07:08 -0700

    Update deps
    Mathlib and everyone else has moved

--- lakefile.lean
+++ lakefile.lean
@@ -1,8 +1,15 @@
 import Lake
 open System Lake DSL
 
+
+
+
 package llm.lean where
   srcDir := "lean"
+  moreLinkArgs := #[
+    "-L./.lake/packages/LeanCopilot/.lake/build/lib",
+    "-lctranslate2"
+  ]
 
 lean_lib LinearAlgebra
 lean_lib Llm
@@ -13,3 +20,7 @@ lean_exe test where
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
+require aesop from git
+  "https://github.com/leanprover-community/aesop.git"
+-- v1.4.1 works with toolchain 4.10.0-rc1
+require LeanCopilot from git "https://github.com/lean-dojo/LeanCopilot.git" @ "v1.4.1"
