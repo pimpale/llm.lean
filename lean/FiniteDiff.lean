3a88699814e4df0f0e9dbe816939f92e5cb5061e
Alok Singh <alokbeniwal@gmail.com>
2024-08-01 23:57:03 -0700

    rm copy of finite diff

--- lean/FiniteDiff.lean
+++
@@ -1,6 +0,0 @@
-import LinearAlgebra.Vector
-
-/-- finite difference approximation of the derivative of a function -/
-def finiteDiff (f : Vector Float n → Vector Float m) (x : Vector Float n) (ε := 1e-5) : Vector Float m :=
-  let dx := x.map (ε * ·)
-  (f (x + dx) - f x) / dx.norm
