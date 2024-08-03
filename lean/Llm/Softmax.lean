491db40a9b9e77fdf6218171cb459045399a3786
Alok Singh <alokbeniwal@gmail.com>
2024-08-02 19:30:32 -0700

    update softmax impl

--- lean/Llm/Softmax.lean
+++ lean/Llm/Softmax.lean
@@ -1,22 +1,30 @@
 import LinearAlgebra.Vector
+import Llm.FiniteDiff
 
 def softmax(x: Vector Float n) : Vector Float n :=
-  let maxv := x.foldl max (-Float.inf)
-  let x := x.map (λ xi => xi - maxv)
+  let maxv := x.foldl max (init := -Float.inf)
+  let x := x.map (· - maxv)
   let exp_x := x.map Float.exp
   let sum_exp_x := exp_x.sum
-  exp_x.map (λ x => x / sum_exp_x)
+  exp_x.map (· / sum_exp_x)
 
 
 def softmax_backward
   (dout: Vector Float n)
   (out: Vector Float n)
 : Vector Float n :=
-  let sum := (dout * out).sum
-  let dx := dout - Vector.of sum
-  out * dx
+  -- -- Compute Jacobian matrix
+  let S : Vector (Vector Float n) n := .ofFn fun r => .ofFn fun c =>
+      if r == c
+      then out[r] * (1 - out[c])
+      else -out[r] * out[c]
+
+  -- Multiply Jacobian with incoming gradient
+  S * dout
+
+#eval finiteDiff softmax !v[1, 2, 3]
 
 #eval softmax !v[1, 2, 3]
 
 
-#eval softmax !v[5]
+#eval softmax_backward !v[1,2,3] (softmax !v[1,2,3])
