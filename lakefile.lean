9247ef2ac6870af1583fbcc58775014e7c3d1ba1
Alok Singh <alokbeniwal@gmail.com>
2024-12-26 19:37:26 -0800

    update lakefile

--- lakefile.lean
+++ lakefile.lean
@@ -3,10 +3,6 @@ open System Lake DSL
 
 package llm.lean where
   srcDir := "lean"
-  -- moreLinkArgs := #[
-  --   "-L./.lake/packages/LeanCopilot/.lake/build/lib",
-  --   "-lctranslate2"
-  -- ]
 
 lean_lib LinearAlgebra
 lean_lib Llm
@@ -15,7 +11,8 @@ lean_lib Llm
 lean_exe test where
   root := `Main
 
-require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"
+require mathlib from git "https://github.com/leanprover-community/mathlib4.git"
 
-require scilean from git "https://github.com/lecopivo/scilean.git" @ "92b1246"
+require scilean from git "https://github.com/lecopivo/scilean.git" @ "1de5c61aba75b496962ba9eb991570bb21169516"
+require plausible from git "https://github.com/leanprover-community/plausible.git"
+require "leanprover-community" / "batteries" @ git "main"
